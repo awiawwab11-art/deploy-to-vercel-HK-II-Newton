81c64956546c133e762e8702053ed0158be474e0
Travkar <awiawwab11@gmail.com>
2025-12-04 15:24:21 +0700

    Delete vercel_pwa_final directory

--- vercel_pwa_final/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  // Untuk Netlify, serve dari root ("/"). Jika Anda hendak melayani dari subpath,
-  // ubah base menjadi '/your-subpath/'.
-  base: '/'
-});
